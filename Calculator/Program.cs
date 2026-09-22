15a45332b4df30f35d447105cff40244ab06a1a0
b01032058 <dallas-w@ulster.ac.uk>
2026-09-22 12:11:32 +0100

    added the 4 normal operators and a while loop to ensure users enter a valid operation choice

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,7 +1,48 @@
-﻿
-Console.WriteLine("Enter the first number:");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter the second number:");
-int secondNumber = Convert.ToInt32(Console.ReadLine());
-int result = firstNumber + secondNumber;
-Console.WriteLine("Adding {0} and {1} gives {2}", firstNumber, secondNumber, result);
+﻿using System.Xml.Serialization;
+
+CalculatorApp();
+void CalculatorApp()
+{
+    int firstNumber = 0;
+    int secondNumber = 0;
+    int result = 0;
+    int choice = 0;
+
+    Console.WriteLine("Enter the first number:");
+    firstNumber = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter the second number:");
+    secondNumber = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Choose an operation: +, -, *, /");
+    Console.WriteLine("1 - Add");
+    Console.WriteLine("2 - Subtract");
+    Console.WriteLine("3 - Multiply");
+    Console.WriteLine("4 - Divide");
+
+    choice = Convert.ToInt32(Console.ReadLine());
+    while (choice < 1 || choice > 4)
+    {
+        Console.WriteLine("Invalid choice - please select a valid option");
+        Console.WriteLine("Choose an operation: +, -, *, /");
+        Console.WriteLine("1 - Add");
+        Console.WriteLine("2 - Subtract");
+        Console.WriteLine("3 - Multiply");
+        Console.WriteLine("4 - Divide");
+        choice = Convert.ToInt32(Console.ReadLine());
+    }
+    if (choice == 1) {
+        result = firstNumber + secondNumber;
+        Console.WriteLine("Adding {0} and {1} gives {2}", firstNumber, secondNumber, result);
+    }
+    else if (choice == 2) {
+        result = firstNumber - secondNumber;
+        Console.WriteLine("Subtracting {0} from {1} gives {2}", secondNumber, firstNumber, result);
+    } else if (choice == 3) {
+        result = firstNumber * secondNumber;
+        Console.WriteLine("Multiplying {0} and {1} gives {2}", firstNumber, secondNumber, result);
+    } else if (choice == 4) {
+        result = firstNumber / secondNumber;
+        Console.WriteLine("Dividing {0} by {1} gives {2}", firstNumber, secondNumber, result);
+    }
+   
+    }
