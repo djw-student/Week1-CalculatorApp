18ff1e1483e47a7c64e24c5830f375497f05170b
b01032058 <dallas-w@ulster.ac.uk>
2026-09-22 11:45:17 +0100

    Implemented program code.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Enter the first number:");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the second number:");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+int result = firstNumber + secondNumber;
+Console.WriteLine("Adding {0} and {1} gives {2}", firstNumber, secondNumber, result);
